a324561dda43e9f2a22e09b7a854c4525d1acb95
Eamonn Watson <Eamonn.Watson@flyacva.com>
2024-01-17 20:26:57 -0400

    Result Updates

--- Result.cs
+++ Result.cs
@@ -4,24 +4,33 @@ public class Result
     public bool IsSuccess => Errors.Count == 0;
     public bool IsFailure => !IsSuccess;
     public List<Error> Errors { get; } = [];
-    protected Result() { }
+    internal Result() { }
     public static Result Ok() => new();
+    public static Result<TData> Ok<TData>(TData value)
+    {
+        ArgumentNullException.ThrowIfNull(value, nameof(value));
+        return new Result<TData>(value);
+    }
+    public static Result Fail(string message) => new Result().WithError(message);
     public static Result Fail(Error error) => new Result().WithError(error);
-    public static Result Fail(string message) => Fail(new Error(message));
     public static Result Fail(IEnumerable<Error> errors) => new Result().WithErrors(errors);
+    public static Result<TData> Fail<TData>(string message) => new Result<TData>(default).WithError(message);
+    public static Result<TData> Fail<TData>(Error error) => new Result<TData>(default).WithError(error);
+    public static Result<TData> Fail<TData>(IEnumerable<Error> errors) => new Result<TData>(default).WithErrors(errors);
+
+    public Result<TNewValue> Bind<TNewValue>(Func<Result<TNewValue>> bindFunction)
+    {
+        if (IsSuccess)
+            return bindFunction();
+
+        return new Result<TNewValue>(default).WithErrors(Errors);
+    }
 }
 
 public class Result<TData> : Result
 {
-    public TData Value { get; }
-    private Result(TData value)
-    {
-        Value = value;
-    }
-    public static Result<TData> Ok(TData value) => new(value);
-    public static Result<TData> Fail(TData value, Error error) => new Result<TData>(value).WithError(error);
-    public static Result<TData> Fail(TData value, string message) => Fail(value, new Error(message));
-    public static Result<TData> Fail(TData value, IEnumerable<Error> errors) => new Result<TData>(value).WithErrors(errors);
+    public TData? Value { get; }
+    internal Result(TData? value) { Value = value; }
 
     public static implicit operator Result<TData>(TData value) => Ok(value);
 }
--- ResultExtensions.cs
+++ ResultExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace EamonnWatson.Common;
+﻿using System.Runtime;
+
+namespace EamonnWatson.Common;
 public static class ResultExtensions
 {
     public static TResult WithError<TResult>(this TResult result, string message) where TResult : Result
@@ -16,5 +18,49 @@ public static class ResultExtensions
     {
         result.Errors.Add(error);
         return result;
+    }
+
+    public static Result<TData> FailIf<TData>(this Result<TData> result, Func<TData, bool> predicate, Error error)
+    {
+        if (result.IsFailure)
+            return result;
+
+        return predicate(result.Value!) 
+            ? Result.Fail<TData>(error) 
+            : result;
+    }
+
+    public static Result<TOut> Map<TIn, TOut>(this Result<TIn> result, Func<TIn, TOut> mappingFunction)
+    {
+        return result.IsSuccess 
+            ? Result.Ok(mappingFunction(result.Value!)) 
+            : Result.Fail<TOut>(result.Errors);
+    }
+
+    public static void Match<TResult>(this TResult result, Action<TResult> onSuccess, Action<TResult> onFailure) where TResult : Result
+    {
+        ArgumentNullException.ThrowIfNull(onSuccess, nameof(onSuccess));
+        ArgumentNullException.ThrowIfNull(onFailure, nameof(onFailure));
+
+        if (result.IsFailure)
+        {
+            onFailure(result);
+            return;
+        }
+
+        onSuccess(result);
+    }
+
+    public static TOut Match<TResult, TOut>(this TResult result, Func<TResult, TOut> onSuccess, Func<TResult, TOut> onFailure) where TResult : Result
+    {
+        ArgumentNullException.ThrowIfNull(onSuccess, nameof(onSuccess));
+        ArgumentNullException.ThrowIfNull(onFailure, nameof(onFailure));
+
+        if (result.IsFailure)
+        {
+            return onFailure(result);
+        }
+
+        return onSuccess(result);
     }
 }
